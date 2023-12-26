d4de3c716bffea3f3e6f9b715d185238af454e52
Achu-Prasad <achuachu61973@gmail.com>
2023-12-26 16:52:42 +0530

    Files updated

--- 3-read-from-file.js
+++ 3-read-from-file.js
@@ -5,3 +5,24 @@
 // Try to do an expensive operation below the file read and see how it affects the output. 
 // Make the expensive operation more and more expensive and see how it affects the output. 
 
+var fs = require ("fs");
+
+fs.readFile('index.js','utf-8',function (err,data){
+    console.log (data);
+})
+
+function calculateTime(n) {
+    const startTime = new Date();
+
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    const endTime = new Date();
+    const elapsedTimeInSeconds = (endTime - startTime) / 1000;
+
+    return  console.log(`Sum from 1 to ${n}: ${sum}`),
+    console.log(`Time taken: ${elapsedTimeInSeconds} seconds`);
+}
+calculateTime(1000000000);
